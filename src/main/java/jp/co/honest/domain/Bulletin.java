15931b543dcbe8693660adae0280970487d62d35
Panhungting <a0910827893@gmail.com>
2021-04-24 00:29:17 +0900

    commit 0424

--- src/main/java/jp/co/honest/domain/Bulletin.java
+++ src/main/java/jp/co/honest/domain/Bulletin.java
@@ -3,15 +3,18 @@ package jp.co.honest.domain;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Bulletin implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
     private Integer bulletinId;
     private String category;
     private String title;
